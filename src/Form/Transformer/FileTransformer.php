6e8356b2f1976f5c1dedea1add63f83b6047d23d
Linus Metzler <linus.metzler@cec.valantic.com>
2021-03-22 12:07:15 +0100

    fix for File constraints

---
+++ src/Form/Transformer/FileTransformer.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Valantic\PimcoreFormsBundle\Form\Transformer;
+
+use Limenius\Liform\Transformer\StringTransformer;
+use Symfony\Component\Form\FormInterface;
+
+class FileTransformer extends StringTransformer
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function transform(FormInterface $form, array $extensions = [], $widget = null)
+    {
+        $schema = ['type' => 'file'];
+
+        return $this->addCommonSpecs($form, $schema, $extensions, $widget);
+    }
+}
