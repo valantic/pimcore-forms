6181d51443b7627fcec7f466c32c89b328282d10
Linus Metzler <linus.metzler@cec.valantic.com>
2021-05-04 08:33:51 +0200

    refactor

--- src/Form/Transformer/FileTransformer.php
+++ src/Form/Transformer/FileTransformer.php
@@ -4,13 +4,10 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreFormsBundle\Form\Transformer;
 
-use Limenius\Liform\Transformer\StringTransformer;
 use Symfony\Component\Form\FormInterface;
 
 class FileTransformer extends StringTransformer
 {
-    use OverwriteAbstractTransformerTrait;
-
     /**
      * {@inheritDoc}
      */
