97c2e4e09eae1f7b3b28ef97e37ab59bea331998
Linus Metzler <linus.metzler@cec.valantic.com>
2022-03-30 13:41:22 +0200

    phpcs

--- src/Form/Extension/FormDataExtension.php
+++ src/Form/Extension/FormDataExtension.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Valantic\PimcoreFormsBundle\Form\Extension;
 
 use Limenius\Liform\Transformer\ExtensionInterface;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\FormInterface;
 
 class FormDataExtension implements ExtensionInterface
