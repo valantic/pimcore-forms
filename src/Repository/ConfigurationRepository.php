9f4d65f6e0f72858db910892686d38affc1fde05
Linus Metzler <hi@linusmetzler.me>
2023-12-27 13:31:53 +0100

    remove unnecessary installer

--- src/Repository/ConfigurationRepository.php
+++ src/Repository/ConfigurationRepository.php
@@ -8,9 +8,7 @@ use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class ConfigurationRepository
 {
-    final public const CONTAINER_TAG = 'valantic.picmore_forms.config';
-    final public const EDITOR_STORAGE_DIRECTORY = PIMCORE_PRIVATE_VAR . '/bundles/valantic-forms';
-    final public const EDITOR_STORAGE_FILE = self::EDITOR_STORAGE_DIRECTORY . '/forms.yml';
+    final public const CONTAINER_TAG = 'valantic.pimcore_forms.config';
 
     public function __construct(
         protected readonly ParameterBagInterface $parameterBag
--- src/ValanticPimcoreFormsBundle.php
+++ src/ValanticPimcoreFormsBundle.php
@@ -5,12 +5,10 @@ declare(strict_types=1);
 namespace Valantic\PimcoreFormsBundle;
 
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
-use Pimcore\Extension\Bundle\Installer\InstallerInterface;
 use Pimcore\Extension\Bundle\Traits\PackageVersionTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Valantic\PimcoreFormsBundle\DependencyInjection\Compiler\ExtensionCompilerPass;
 use Valantic\PimcoreFormsBundle\DependencyInjection\Compiler\TransformerCompilerPass;
-use Valantic\PimcoreFormsBundle\Installer\Installer;
 
 class ValanticPimcoreFormsBundle extends AbstractPimcoreBundle
 {
@@ -31,16 +29,5 @@ class ValanticPimcoreFormsBundle extends AbstractPimcoreBundle
         }
 
         return json_decode($composer, flags: \JSON_THROW_ON_ERROR)->name;
-    }
-
-    public function getInstaller(): ?InstallerInterface
-    {
-        $installer = $this->container?->get(Installer::class);
-
-        if (!$installer instanceof InstallerInterface) {
-            return null;
-        }
-
-        return $installer;
     }
 }
