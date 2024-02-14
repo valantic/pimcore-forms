c0ee2e8957cddaa758f576c74df4eb2b21a9c2b4
Linus Metzler <hi@linusmetzler.me>
2024-02-14 08:34:16 +0100

    refactor existing simple repos

--- src/Repository/InputHandlerRepository.php
+++ src/Repository/InputHandlerRepository.php
@@ -4,55 +4,13 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreFormsBundle\Repository;
 
-use Valantic\PimcoreFormsBundle\Exception\UnknownInputHandlerException;
 use Valantic\PimcoreFormsBundle\Form\InputHandler\InputHandlerInterface;
 
-class InputHandlerRepository
+/**
+ * @extends AbstractRepository<InputHandlerInterface>
+ *
+ * @internal
+ */
+class InputHandlerRepository extends AbstractRepository
 {
-    /**
-     * @var array<string,InputHandlerInterface>
-     */
-    protected array $inputHandlers;
-
-    /**
-     * @param iterable<InputHandlerInterface> $inputHandlers
-     */
-    public function __construct(iterable $inputHandlers)
-    {
-        $this->inputHandlers = $this->iterableToArray($inputHandlers);
-    }
-
-    public function get(string $key): InputHandlerInterface
-    {
-        if (str_starts_with($key, '\\')) {
-            $key = substr($key, 1);
-        }
-
-        if (!array_key_exists($key, $this->inputHandlers)) {
-            throw new UnknownInputHandlerException($key);
-        }
-
-        return clone $this->inputHandlers[$key];
-    }
-
-    /**
-     * @param iterable<object> $iterables
-     *
-     * @return array<string,InputHandlerInterface>
-     */
-    public function iterableToArray(iterable $iterables): array
-    {
-        $arr = [];
-
-        foreach ($iterables as $iterable) {
-            if (!($iterable instanceof InputHandlerInterface)) {
-                continue;
-            }
-
-            $name = $iterable::class;
-            $arr[$name] = $iterable;
-        }
-
-        return $arr;
-    }
 }
--- src/Repository/RedirectHandlerRepository.php
+++ src/Repository/RedirectHandlerRepository.php
@@ -4,55 +4,13 @@ declare(strict_types=1);
 
 namespace Valantic\PimcoreFormsBundle\Repository;
 
-use Valantic\PimcoreFormsBundle\Exception\UnknownRedirectHandlerException;
 use Valantic\PimcoreFormsBundle\Form\RedirectHandler\RedirectHandlerInterface;
 
-class RedirectHandlerRepository
+/**
+ * @extends AbstractRepository<RedirectHandlerInterface>
+ *
+ * @internal
+ */
+class RedirectHandlerRepository extends AbstractRepository
 {
-    /**
-     * @var array<string,RedirectHandlerInterface>
-     */
-    protected array $redirectHandlers;
-
-    /**
-     * @param iterable<RedirectHandlerInterface> $redirectHandlers
-     */
-    public function __construct(iterable $redirectHandlers)
-    {
-        $this->redirectHandlers = $this->iterableToArray($redirectHandlers);
-    }
-
-    public function get(string $key): RedirectHandlerInterface
-    {
-        if (str_starts_with($key, '\\')) {
-            $key = substr($key, 1);
-        }
-
-        if (!array_key_exists($key, $this->redirectHandlers)) {
-            throw new UnknownRedirectHandlerException($key);
-        }
-
-        return clone $this->redirectHandlers[$key];
-    }
-
-    /**
-     * @param iterable<object> $iterables
-     *
-     * @return array<string,RedirectHandlerInterface>
-     */
-    public function iterableToArray(iterable $iterables): array
-    {
-        $arr = [];
-
-        foreach ($iterables as $iterable) {
-            if (!($iterable instanceof RedirectHandlerInterface)) {
-                continue;
-            }
-
-            $name = $iterable::class;
-            $arr[$name] = $iterable;
-        }
-
-        return $arr;
-    }
 }
