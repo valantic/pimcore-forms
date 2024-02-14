4c47deaf7b75f9d57e1d4aa64392360bfe2e125d
Linus Metzler <hi@linusmetzler.me>
2024-02-14 08:34:08 +0100

    introduce repo for choices

---
+++ src/Repository/AbstractRepository.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Valantic\PimcoreFormsBundle\Repository;
+
+use Valantic\PimcoreFormsBundle\Exception\Repository\ItemNotFoundInRepositoryException;
+
+/**
+ * @template TItem
+ *
+ * @internal
+ */
+abstract class AbstractRepository
+{
+    /** @var TItem[] */
+    protected array $items;
+
+    public function __construct(
+        /** @var \Iterator<TItem> */
+        protected iterable $iterables,
+    ) {
+    }
+
+    /** @return TItem */
+    public function get(string $key)
+    {
+        return $this->all()[$key] ?? throw new ItemNotFoundInRepositoryException($key);
+    }
+
+    /** @return TItem[] */
+    protected function all(): array
+    {
+        $this->items ??= $this->initializeItemsFromIterables();
+
+        return $this->items;
+    }
+
+    /**
+     * @return array<class-string<TItem>,TItem>
+     */
+    protected function initializeItemsFromIterables(): array
+    {
+        $arr = [];
+
+        foreach ($this->iterables as $iterable) {
+            $arr[$iterable::class] = $iterable;
+        }
+
+        return $arr;
+    }
+}
---
+++ src/Repository/ChoicesRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Valantic\PimcoreFormsBundle\Repository;
+
+use Valantic\PimcoreFormsBundle\Form\Type\ChoicesInterface;
+
+/**
+ * @extends AbstractRepository<ChoicesInterface>
+ *
+ * @internal
+ */
+class ChoicesRepository extends AbstractRepository
+{
+}
